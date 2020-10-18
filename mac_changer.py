8f50087608f394be63a954e81a970c5fab916d6f
jvb <jbumba75@gmail.com>
2020-10-18 15:30:50 +0100

    added more code

--- mac_changer.py
+++ mac_changer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import subprocess
 import optparse
+import re
 
 def get_arguments():
     parser = optparse.OptionParser()
@@ -16,9 +17,26 @@ def get_arguments():
 
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
-    # subprocess.call(["ifconfig", interface, "down"])
-    # subprocess.call(["ifconfig", interface, "hw", "ether", mac_new])
-    # subprocess.call(["ifconfig", interface, "up"])
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
+
 
 options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
 change_mac(options.interface, options.new_mac)
+
+current_mac = get_current_mac(options.interface)
+if current_mac == options.interface:
+    print("[+] MAC address was successfully changed to" + current_mac)
+else:
+    print("MAC did not get changed")
